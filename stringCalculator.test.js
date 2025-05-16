5d2a6605e88192a8e816161a991e87d516f0df41
Shivam Gullu <shivamgullu@INLT-SGGULATI.local>
2025-05-16 22:19:57 +0530

    delimiters and negative numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -19,3 +19,11 @@ test('handles any amount of comma-separated numbers', () => {
 test('handles newlines as delimiters', () => {
   expect(add('1\n2,3')).toBe(6);
 });
+
+test('supports custom delimiter', () => {
+  expect(add('//;\n1;2')).toBe(3);
+});
+
+test('throws on negative numbers', () => {
+  expect(() => add('1,-2,-3')).toThrow('Negative numbers not allowed: -2, -3');
+});
