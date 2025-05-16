f9616c900d5d0ecacbe2f8bb723ba1af54fb5375
Shivam Gullu <shivamgullu@INLT-SGGULATI.local>
2025-05-16 22:20:31 +0530

    delimiters and negative numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -9,7 +9,11 @@ function add(numbers) {
     }
   
     const numArray = numbers.split(delimiter).map(Number);
-
+  
+    const negatives = numArray.filter(n => n < 0);
+    if (negatives.length > 0) {
+      throw new Error(`Negative numbers not allowed: ${negatives.join(', ')}`);
+    }
   
     return numArray.reduce((sum, n) => sum + n, 0);
   }
